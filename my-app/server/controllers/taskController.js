ec1bef8cbf0ff4ff2637ef39c624c0a1a46b6328
louislam <louislam7229@gmail.com>
2022-10-02 02:10:08 -0700

    filled out taskController, post, get

--- my-app/server/controllers/taskController.js
+++ my-app/server/controllers/taskController.js
@@ -1,1 +1,34 @@
-module.exports = {};
+const models = require('../models/TaskModel');
+
+module.exports = {
+  getTask(req, res, next) {
+    models.Task.find({})
+      .then((response) => {
+        res.locals.tasks = response;
+        return next();
+      })
+      .catch((err) => {
+        console.log('getTask error', err);
+        return next(err);
+      });
+  },
+
+  //end of getTask
+
+  postTask(req, res, next) {
+    const { task } = req.body;
+    const time = Date.now().toString();
+    models.Task.create({ item: task, created_at: time })
+      .then((response) => {
+        console.log(response);
+        res.locals.item = response;
+        return next();
+      })
+      .catch((err) => {
+        console.log('postTask error', err);
+        return next(err);
+      });
+  },
+
+  //end of postTask
+};
